8f241fd5c15dc3950013e8882d63d53693ccea4b
lytree <hi.yangzh@outlook.com>
2023-03-12 20:58:33 +0800

    delete

--- Wave.Tool/SQLite.cs
+++ Wave.Tool/SQLite.cs
@@ -10,62 +10,28 @@ using System.Threading.Tasks;
 
 namespace Wave.Tool;
 
-<<<<<<< HEAD
-		public static List<Dictionary<string, object>> ReadCMSSQLite(string path)
-		{
-			IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				   .Build();
-			return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
-		}
-		public static List<Dictionary<string, object>> ReadTMSSQLite(string path)
-		{
-			IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				   .Build();
-			return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
-		}
-		public static List<Dictionary<string, object>> ReadBMSSQLite(string path)
-		{
-			IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				   .Build();
-			return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
-		}
-=======
 public class SQLite
 {
-	public SQLite() { }
-
-	public static List<CMS.vibdata> ReadCMSSQLite(string path)
+	public static List<Dictionary<string, object>> ReadCMSSQLite(string path)
 	{
-		using IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			.UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				.Build();
-		var list = fsql.Select<CMS.vibdata>().ToList();
-
-		return list;
->>>>>>> b830519e2efec72becf01ae3a280532a8887a16b
+		IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
+		   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
+			   .Build();
+		return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
 	}
-	public static List<TMS.vibdata> ReadTMSSQLite(string path)
+	public static List<Dictionary<string, object>> ReadTMSSQLite(string path)
 	{
-		using IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			.UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				.Build();
-		var list = fsql.Select<TMS.vibdata>().ToList();
-
-		return list;
+		IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
+		   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
+			   .Build();
+		return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
 	}
-	public static List<BMS.vibdata> ReadBMSSQLite(string path)
+	public static List<Dictionary<string, object>> ReadBMSSQLite(string path)
 	{
-		using IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
-			.UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
-				.Build();
-		var list = fsql.Select<BMS.vibdata>().ToList();
-		fsql.Dispose();
-		return list;
+		IFreeSql fsql = new FreeSqlBuilder().UseConnectionString(DataType.Sqlite, $@"Data Source={path}")
+		   .UseMonitorCommand(cmd => Console.Write(cmd.CommandText))
+			   .Build();
+		return fsql.Ado.Query<Dictionary<string, object>>("select * from main.vibdata").ToList();
 	}
-
 
 }
-
